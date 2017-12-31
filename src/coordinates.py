bfb862a22e1768fcd70f6a3e4f2e6eee8d3ccaa8
Adam Buran <aburan28@gmail.com>
2017-12-31 06:20:05 -0800

    Update coordinates.py

---
+++ src/coordinates.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env pyth
+on
+
+
+class Point(object):
+    def __init__(self,x,y):
+        self._x = x
+        self._y = y
+    def __add__(self,):
+    
+    
+    def __mul__
+    def __sub__
+class JacobianPoint(Point):
+
